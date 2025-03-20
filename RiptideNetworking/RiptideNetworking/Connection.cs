e63f22ba21e5e07c72fdd8896d68132ec76302b6
Per6 <per.daude@t-online.de>
2025-03-20 08:41:57 +0100

    merged

--- RiptideNetworking/RiptideNetworking/Connection.cs
+++ RiptideNetworking/RiptideNetworking/Connection.cs
@@ -181,7 +181,8 @@ namespace Riptide
         /// <returns>For reliable, queued and notify messages, the sequence ID that the message was sent with. 0 for unreliable messages.</returns>
         public ushort Send(Message message)
         {
-			if((this is TcpConnection && message.Header.IsUserMessage()) || (this is BluetoothConnection && message.Header == MessageHeader.Reliable))
+			if((this is TcpConnection && message.Header.IsUserMessage())
+				|| (this is BluetoothConnection && message.Header == MessageHeader.Reliable))
 				message.SetSendHeader(MessageHeader.Unreliable, message.Id);
 			MessageSendMode sendMode = message.SetSendHeader();
 			if(message.BytesInUse >= Message.MaxSize) throw new Exception($"Message is too large to send {message.BytesInUse} with max of {Message.MaxSize}. Consider splitting it up or increasing Message.MaxPayloadSize at the cost of either reliability or resend attempts.");
